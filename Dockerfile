d9dd956ba66062491d63f856bcf603f5fbcbae76
Raul Flores <raul.flores@ciberc.com>
2020-11-09 17:26:23 -0600

    Se agrega CSS, Javascript asi como se edito html para practicas

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM nginx:latest
 #WORKDIR /usr/share/nginx/html
 COPY ./index.html /usr/share/nginx/html
+
---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Javascript document
+/*
+Ejemplo de Javascript en el curso
+para practicas.
+*/
+function saludo() {
+    alert("hola mundo desde javascript en un archivo independiente");
+}
